4d4c7061b6d86354f386a3d1521cccf0837f55ce
yohanstra <jreinstra@gmail.com>
2016-02-03 22:00:41 -0800

    added export feature

--- application/admin.py
+++ application/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 from application.models import Application, Profile
 
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+
+from application.models import Application, Profile
+
 # Register your models here.
-admin.site.register(Application)
+class ApplicationResource(resources.ModelResource):
+    class Meta:
+        model = Application
+        
+        
+class ApplicationAdmin(ImportExportModelAdmin):
+    resource_class = ApplicationResource
+    pass
+
+
+admin.site.register(Application, ApplicationAdmin)
 admin.site.register(Profile)
