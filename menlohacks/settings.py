d4e9ae01783cf8a06b2c3407d8e81ba1018b72e6
Thomas Woodside <thomas.woodside@gmail.com>
2016-12-08 07:06:13 -0800

    Heroku.

--- menlohacks/settings.py
+++ menlohacks/settings.py
@@ -123,8 +123,8 @@ LOGIN_REDIRECT_URL = "/"
 # Heroku settings below
 #Parse database configuration from $DATABASE_URL
 # import dj_database_url
-# DATABASES = {}
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
